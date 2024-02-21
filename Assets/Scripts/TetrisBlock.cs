89fd2ff610be8d5fa1713d210e94398c3d7a0439
LeeGunHyung <154494501+Woodong96@users.noreply.github.com>
2024-02-21 15:53:24 +0900

    1

---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+
+    private float previousTime;
+    public float fallTime = 1.0f;
+    public static int height = 20;
+    public static int width = 10;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+            previousTime = Time.time;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
+            {
+                return false;
+            }
+        }
+       
+        return true;
+    }
+}
